18be4dd9824d49de331208d2837dcc8fd25a6c8c
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-30 12:36:12 +0000

    sm8250-common: build custom AIDL awinic vibrator HAL
    Change-Id: I70d7866730f46cd58a79e3b8c398531ab2e8ece8
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- kona.mk
+++ kona.mk
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.xiaomi-awinic
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
