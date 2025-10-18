a36ccb3b1cc15c0f3775a41a013b6fe94f5fa538
kleidione Freitas <kleidione@gmail.com>
2025-10-17 22:49:52 -0300

    sm8250-common: Link more libs against v33 libstagefright_foundation
    * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: I7be89616d092b1af07939bc5239c425d15d4f230
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -34,7 +34,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('dolby', 'default1'),
     (
         'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service',
-        'vendor/bin/hw/vendor.dolby.media.c2@1.0-service', 
+        'vendor/bin/hw/vendor.dolby.media.c2@1.0-service',
+        'vendor/lib64/libdlbdsservice.so',
+        'vendor/lib64/libdlbpreg.so',
+        'vendor/lib64/soundfx/libdlbvol.so',
+        'vendor/lib64/soundfx/libhwdap.so' ,
     ): blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
 }  # fmt: skip
